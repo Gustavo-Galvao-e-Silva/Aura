bd0840cb91c18c97012c2c8480a9ee2aa59fb0a5
horyzon <kalyan.coliveira@gmail.com>
2026-03-07 18:54:55 -0300

    does correct port mapping and url def to connect fastapi srv to db

--- src/server/my_fastapi_app/app/db/session.py
+++ src/server/my_fastapi_app/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Uses the credentials from your docker-compose
-DATABASE_URL = "postgresql://postgres:example@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:example@127.0.0.1:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
