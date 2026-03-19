5313d70282ee0d770032663ade9815f162bd9984
horyzon <kalyan.coliveira@gmail.com>
2026-03-18 22:00:05 -0300

    moves database url definition to .env integration

--- src/server/my_fastapi_app/app/db/session.py
+++ src/server/my_fastapi_app/app/db/session.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Uses the credentials from your docker-compose
-DATABASE_URL = "postgresql://postgres:example@127.0.0.1:5432/postgres"
+# The .env was already loaded by main.py!
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
