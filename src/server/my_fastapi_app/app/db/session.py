859095dd5e2c5ea65438ad0f85a738f6f0afd209
horyzon <kalyan.coliveira@gmail.com>
2026-03-20 18:06:28 -0300

    applies partial of a1dc042de590
    basically, I hated the changes in agents.py, and I will have to work on
    the normalization changes that you did in main.py, because the routes
    are now split.
    
    it would be interesting to see if we can abstract the process of
    normalization that a1dc042de590 did in the main.py (the main.py before
    it was refactored and split) to not have to repeat ourselves every
    single time. a helper function that calls to the normalization helpers
    should suffice

--- src/server/my_fastapi_app/app/db/session.py
+++ src/server/my_fastapi_app/app/db/session.py
@@ -4,6 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 # The .env was already loaded by main.py!
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    db_user = os.getenv("DB_USER", "postgres")
+    db_password = os.getenv("DB_PASSWORD", "example")
+    db_host = os.getenv("DB_HOST", "127.0.0.1")
+    db_port = os.getenv("DB_PORT", "5432")
+    db_name = os.getenv("DB_NAME", "postgres")
+    DATABASE_URL = (
+        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    )
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
